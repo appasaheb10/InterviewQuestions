12ae90296b6c86ca8312a58b0967dfc3e35fedf2
appasaheb10 <appasaheb.be10@gmail.com>
2022-06-11 18:12:29 +0530

    InterviewPractice

---
+++ src/main/java/thread/BlockingQueueDemo.java
@@ -0,0 +1,83 @@
+package thread;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        BlockingQueue<String> queue = new ArrayBlockingQueue<>(5);
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+        Thread producerThread = new Thread(producer);
+        Thread consumerThread = new Thread(consumer);
+
+        producerThread.start();
+        Thread.sleep(5000);
+        //consumerThread.start();
+
+
+    }
+
+    static class Producer implements Runnable {
+
+        BlockingQueue<String> queue=null;
+
+        public Producer(BlockingQueue queue) {
+            super();
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                System.out.println("Producing element 1");
+                queue.put("Element 1");
+               // Thread.sleep(1000);
+                System.out.println("Producing element 2");
+                queue.put("Element 2");
+                //Thread.sleep(1000);
+                System.out.println("Producing element 3");
+                queue.put("Element 3");
+                System.out.println("Producing element 4");
+                queue.put("Element 4");
+                System.out.println("Producing element 5");
+                queue.put("Element 5");
+
+                System.out.println("Producing element 6");
+                queue.put("Element 6");
+                System.out.println("Producing element 7");
+                queue.put("Element 7");
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class Consumer implements Runnable {
+
+        BlockingQueue<String> queue=null;
+
+        public Consumer(BlockingQueue queue) {
+            super();
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+
+            while(true)
+            {
+                try {
+                    System.out.println("Consumed "+queue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
---
+++ src/main/java/thread/CountDownLatchDemo.java
@@ -0,0 +1,63 @@
+package thread;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Java Program to demonstrate how to use CountDownLatch, * Its used when a thread * needs to wait for other threads before starting its work. * * @author Javin Paul
+ */
+public class CountDownLatchDemo {
+    List<String> stringList = Arrays.asList("A", "B");
+    public static void main(String args[]) throws InterruptedException {
+
+        CountDownLatch latch = new CountDownLatch(1);
+        ThreadGroup threadGroup = new ThreadGroup("FileSearch");
+        Worker first = new Worker(1000, latch, "WORKER-1", threadGroup);
+        Worker second = new Worker(2000, latch, "WORKER-2",  threadGroup);
+    /*    Worker third = new Worker(3000, latch, "WORKER-3");
+        Worker fourth = new Worker(4000, latch, "WORKER-4");*/
+        first.start();
+        second.start();
+        /*third.start();
+        fourth.start();*/
+        // Main thread will wait until all thread finished
+         latch.await();
+        threadGroup.interrupt();
+        System.out.println(Thread.currentThread().getThreadGroup());
+
+
+        System.out.println(Thread.currentThread().getName() + " has finished");
+    }
+}
+
+class Worker extends Thread {
+    private int delay;
+    private CountDownLatch latch;
+    List<String> stringList = Arrays.asList("A", "B");
+    public Worker(int delay, CountDownLatch latch, String name, ThreadGroup group) {
+        super(group,name);
+        this.delay = delay;
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " has started");
+            Thread.sleep(delay);
+            if(stringList.contains("B")){
+                latch.countDown();
+            }
+           /* if(latch.getCount() == 0) {
+                Thread.currentThread().getThreadGroup().destroy();
+            }*/
+            System.out.println(Thread.currentThread().getName() + " has finished");
+
+        } catch (InterruptedException e) {
+            System.out.println("Thred intrupt");
+        }
+    }
+}
